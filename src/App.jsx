c7c67e8b7f0bc74eb444c4e3b879a636d5a3f938
MojammelJoy <mojammeljoy2687@gmail.com>
2026-01-11 23:37:35 +0600

    create hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/hero'
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className='dark:bg-black relative'>
      <Navbar theme={theme} setTheme={setTheme}></Navbar>
+     <Hero></Hero>
     </div>
   )
 }
