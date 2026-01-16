2bfad8bf201240043b8aeab2d96927d20f61e5ad
MojammelJoy <mojammeljoy2687@gmail.com>
2026-01-16 22:43:09 +0600

    ourwork section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/hero'
 import TrustedBy from './components/TrustedBy'
 import Services from './components/Services'
+import OurWork from './components/OurWork'
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
      <Hero></Hero>
      <TrustedBy></TrustedBy>
      <Services></Services>
+     <OurWork></OurWork>
     </div>
   )
 }
